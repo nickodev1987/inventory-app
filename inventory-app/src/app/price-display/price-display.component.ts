438eade7e12561b0cfae22818c1816aa4bb53b08
Marlon <nickoo1987@gmail.com>
2018-05-17 00:14:54 -0300

    showing product details

---
+++ inventory-app/src/app/price-display/price-display.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+selector: 'price-display',
+  template: `
+   <div class="price-display">\${{ price }}</div>
+   `,
+  styleUrls: ['./price-display.component.css']
+})
+export class PriceDisplayComponent implements OnInit {
+
+  @Input() price: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
